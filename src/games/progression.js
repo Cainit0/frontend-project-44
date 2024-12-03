a3af03546b6b8077d43cf992ce0bdc3a3b944a26
Cainito <nikita.shakirzhanov@gmail.com>
2024-12-03 14:25:49 +0400

    add games/progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+import { getRandomNum, playGame} from '../index.js';
+
+const question = 'What number is missing in the progression?';
+
+const playProgressionRound = () => {
+    const progressionLength = getRandomNum(5, 10);
+    const progression = [];
+    progression[0] = getRandomNum(1, 30);
+    const progressionStep = getRandomNum(1, 10);
+    for (let i = 1; i < progressionLength; i += 1) {
+        progression[i] = progression[0] + progressionStep * i;
+    }
+
+    const hiddenElement = getRandomNum(0, progressionLength - 1);
+    const correctAnswer = progression[hiddenElement];
+    progression[hiddenElement] = '..';
+
+    console.log(`Question: ${progression.join(' ')}`);
+    const answer = parseInt(readlineSync.question(`Your answer: `));
+    const result = (answer === correctAnswer);
+
+    if (result) {
+        console.log(`Correct!`);
+    } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    }
+
+    return result;
+};
+
+export default () => {
+    playGame(playProgressionRound, question);
+}
